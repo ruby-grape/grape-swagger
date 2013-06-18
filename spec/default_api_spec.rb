033bc8013426b774da553375315f1d1ef1c016d0
dblock <dblock@dblock.org>
2013-06-18 09:56:15 -0400

    Fix: document default/root APIs.

---
+++ spec/default_api_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Default API" do
+
+  before(:all) do
+    class NotAMountedApi < Grape::API
+      desc 'this gets something'
+      get '/something' do
+        {:bla => 'something'}
+      end
+      add_swagger_documentation
+    end
+  end
+
+  def app; NotAMountedApi; end
+
+  it "should document something" do
+    get '/swagger_doc'
+    last_response.body.should == "{:apiVersion=>\"0.1\", :swaggerVersion=>\"1.1\", :basePath=>\"http://example.org\", :operations=>[], :apis=>[{:path=>\"/swagger_doc/something.{format}\"}, {:path=>\"/swagger_doc/swagger_doc.{format}\"}]}"
+  end
+
+end

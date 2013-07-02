bde2bb4c47fbc5eedcffcc2654a31479348d6570
Timo Sulg <timgluz@gmail.com>
2013-07-03 00:02:45 +0200

    restructured code;

---
+++ lib/grape_swagger_core.rb
@@ -0,0 +1,59 @@
+require_relative 'helpers/parser_helpers'
+require 'grape_swagger_handlers'
+
+module GrapeSwaggerCore
+  @@combined_routes = {}
+  SWAGGER_DEFAULTS = {
+    :target_class => nil,
+    :mount_path => '/swagger_doc',
+    :base_path => nil,
+    :api_version => '0.1',
+    :markdown => false,
+    :hide_documentation_path => false,
+    :hide_format => false
+  }
+
+  def combined_routes
+    @@combined_routes
+  end
+
+  def add_combined_routes(routes)
+    routes.each do |route|
+      resource = route.route_path.match('\/(\w*?)[\.\/\(]').captures.first
+      next if resource.empty?
+      resource.downcase!
+      version = route.route_version.to_s
+      @@combined_routes[version] ||= {}
+      @@combined_routes[version][resource] ||= []
+      @@combined_routes[version][resource] << route
+    end
+  end
+
+  def add_swagger_documentation(options={})
+    options = {:target_class => self}.merge(options)
+    options = SWAGGER_DEFAULTS.merge(options)
+    
+    documentation_class = create_documentation_class
+    documentation_class.register_handlers(options)
+
+    mount(documentation_class)
+
+    add_combined_routes(routes)
+    p "#-----------", combined_routes
+  end
+
+  def create_documentation_class
+    Class.new(Grape::API) do
+      extend GrapeSwaggerHandlers
+
+      class << self
+        def name
+          @@class_name
+        end
+      end
+
+      helpers ParserHelpers
+    end
+  end
+  #end of create_docu
+end
---
+++ lib/grape_swagger_handlers.rb
@@ -0,0 +1,88 @@
+=begin
+  GrapeSwaggerHandlers module includes functions that
+  register handlers for GrapeSwagger doc views.
+=end
+
+module GrapeSwaggerHandlers
+	def register_handlers(options)
+    @@target_class = options[:target_class]
+    @@mount_path = options[:mount_path]
+    @@class_name = options[:class_name] || options[:mount_path].gsub('/','')
+    @@markdown = options[:markdown]
+    @@hide_documentation_path = options[:hide_documentation_path]
+    @@hide_format = options[:hide_format]
+    @@api_version = options[:api_version]
+
+    @@base_path = options[:base_path]
+
+		register_root_handler
+    register_resource_handler
+	end
+
+	def register_root_handler()
+	    desc 'Swagger compatible API description'
+	    get @@mount_path do
+	      header['Access-Control-Allow-Origin'] = '*'
+	      header['Access-Control-Request-Method'] = '*'
+	      routes = @@target_class::combined_routes[@@api_version]
+
+	      if @@hide_documentation_path
+	        routes.reject!{ |route, value| "/#{route}/".index(parse_path(@@mount_path, nil) << '/') == 0 }
+	      end
+
+	      routes_array = routes.keys.map do |local_route|
+	          { :path => "#{parse_path(route.route_path.gsub('(.:format)', ''),route.route_version)}/#{local_route}#{@@hide_format ? '' : '.{format}'}" }
+	      end
+
+	      {
+	        apiVersion: @@api_version,
+	        swaggerVersion: "1.1",
+	        basePath: parse_base_path(@@base_path, request),
+	        operations:[],
+	        apis: routes_array
+	      }
+	    end
+	end
+
+  def register_resource_handler
+    desc 'Swagger compatible API description for specific API',
+          :params =>{
+            "name" => {
+              :desc => "Resource name of mounted API",
+              :type => "string",
+              :required => true },
+          }
+    get "#{@@mount_path}/:name" do
+      header['Access-Control-Allow-Origin'] = '*'
+      header['Access-Control-Request-Method'] = '*'
+
+      routes = @@target_class::combined_routes[@@api_version][params[:name]]
+
+      routes_array = routes.map do |route|
+        notes = route.route_notes && @@markdown ? Kramdown::Document.new(strip_heredoc(route.route_notes)).to_html : route.route_notes
+        http_codes = parse_http_codes route.route_http_codes
+        operations = {
+            :notes => notes,
+            :summary => route.route_description || '',
+            :nickname   => route.route_method + route.route_path.gsub(/[\/:\(\)\.]/,'-'),
+            :httpMethod => route.route_method,
+            :parameters => parse_header_params(route.route_headers) +
+              parse_params(route.route_params, route.route_path, route.route_method)
+        }
+        operations.merge!({:errorResponses => http_codes}) unless http_codes.empty?
+        {
+          :path => parse_path(route.route_path, @@api_version),
+          :operations => [operations]
+        }
+      end
+
+      {
+        apiVersion: @@api_version,
+        swaggerVersion: "1.1",
+        basePath: parse_base_path(@@base_path, request),
+        resourcePath: "",
+        apis: routes_array
+      }
+    end
+  end
+end

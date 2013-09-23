914ca47cbb87b9e077e29ddb427eada6ed1dfe69
Caleb Woods <calebawoods@gmail.com>
2013-09-23 14:21:07 -0400

    Hack to correctly support base path

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -231,7 +231,7 @@ module Grape
             end
 
             def parse_base_path(base_path, request)
-              (base_path.is_a?(Proc) ? base_path.call(request) : base_path) || request.base_url
+              (base_path.is_a?(Proc) ? base_path.call(request) : base_path) || "#{request.url}/api"
             end
           end
         end

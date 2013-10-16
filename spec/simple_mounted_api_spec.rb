14677cf128c9294343c7395ee7f45024743957b3
Adriano Dadario <adriano.dadario@vagas.com.br>
2013-10-16 15:10:58 -0300

    Removing unexpect END

--- spec/simple_mounted_api_spec.rb
+++ spec/simple_mounted_api_spec.rb
@@ -142,61 +142,59 @@ describe "a simple mounted api" do
     }
   end
 
-    it "includes headers" do
-      get '/swagger_doc/simple_with_headers.json'
-      JSON.parse(last_response.body)["apis"].should == [{
-        "path" => "/simple_with_headers.{format}",
-        "operations" => [
-          {
-            "notes" => nil,
-            "summary" => "this gets something else",
-            "nickname" => "GET-simple_with_headers---format-",
-            "httpMethod" => "GET",
-            "parameters" => [
-              { "paramType" => "header", "name" => "XAuthToken", "description" => "A required header.", "dataType" => "String", "required" => true },
-              { "paramType" => "header", "name" => "XOtherHeader", "description" => "An optional header.", "dataType" => "String", "required" => false }
-            ],
-            "errorResponses" => [
-              { "code" => 403, "reason" => "invalid pony" },
-              { "code" => 405, "reason" => "no ponies left!" }
-            ]
-          }
-        ]
-      }]
-    end
+  it "includes headers" do
+    get '/swagger_doc/simple_with_headers.json'
+    JSON.parse(last_response.body)["apis"].should == [{
+      "path" => "/simple_with_headers.{format}",
+      "operations" => [
+        {
+          "notes" => nil,
+          "summary" => "this gets something else",
+          "nickname" => "GET-simple_with_headers---format-",
+          "httpMethod" => "GET",
+          "parameters" => [
+            { "paramType" => "header", "name" => "XAuthToken", "description" => "A required header.", "dataType" => "String", "required" => true },
+            { "paramType" => "header", "name" => "XOtherHeader", "description" => "An optional header.", "dataType" => "String", "required" => false }
+          ],
+          "errorResponses" => [
+            { "code" => 403, "reason" => "invalid pony" },
+            { "code" => 405, "reason" => "no ponies left!" }
+          ]
+        }
+      ]
+    }]
+  end
 
-    it "supports multiple parameters" do
-      get '/swagger_doc/items.json'
-      JSON.parse(last_response.body)["apis"].should == [{
-        "path" => "/items.{format}",
-        "operations" => [
-          {
-            "notes" => nil,
-            "summary" => "this takes an array of parameters",
-            "nickname" => "POST-items---format-",
-            "httpMethod" => "POST",
-            "parameters" => [ { "paramType" => "form", "name" => "items[]", "description" => "array of items", "dataType" => "String", "required" => false } ]
-          }
-        ]
-      }]
-    end
+  it "supports multiple parameters" do
+    get '/swagger_doc/items.json'
+    JSON.parse(last_response.body)["apis"].should == [{
+      "path" => "/items.{format}",
+      "operations" => [
+        {
+          "notes" => nil,
+          "summary" => "this takes an array of parameters",
+          "nickname" => "POST-items---format-",
+          "httpMethod" => "POST",
+          "parameters" => [ { "paramType" => "form", "name" => "items[]", "description" => "array of items", "dataType" => "String", "required" => false } ]
+        }
+      ]
+    }]
+  end
 
-    it "supports custom types" do
-      get '/swagger_doc/custom.json'
-      JSON.parse(last_response.body)["apis"].should == [{
-        "path" => "/custom.{format}",
-        "operations" => [
-          {
-            "notes" => nil,
-            "summary" => "this uses a custom parameter",
-            "nickname" => "GET-custom---format-",
-            "httpMethod" => "GET",
-            "parameters" => [ { "paramType" => "query", "name" => "custom", "description" => "array of items", "dataType" => "CustomType", "required" => false } ]
-          }
-        ]
-      }]
-    end
-
+  it "supports custom types" do
+    get '/swagger_doc/custom.json'
+    JSON.parse(last_response.body)["apis"].should == [{
+      "path" => "/custom.{format}",
+      "operations" => [
+        {
+          "notes" => nil,
+          "summary" => "this uses a custom parameter",
+          "nickname" => "GET-custom---format-",
+          "httpMethod" => "GET",
+          "parameters" => [ { "paramType" => "query", "name" => "custom", "description" => "array of items", "dataType" => "CustomType", "required" => false } ]
+        }
+      ]
+    }]
   end
 
 end

d155810667c6380782f3692870bbad5d7699c631
dB <dblock@dblock.org>
2014-12-01 08:59:20 -0500

    Fix: grape HEAD after https://github.com/intridea/grape/pull/831.

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -32,6 +32,7 @@ module Grape
 
         @combined_namespaces = {}
         combine_namespaces(self)
+        documentation_class
       end
 
       private
@@ -55,177 +56,6 @@ module Grape
             def name
               @@class_name
             end
-          end
-
-          def self.setup(options)
-            defaults = {
-              target_class: nil,
-              mount_path: '/swagger_doc',
-              base_path: nil,
-              api_version: '0.1',
-              markdown: nil,
-              hide_documentation_path: false,
-              hide_format: false,
-              format: nil,
-              models: [],
-              info: {},
-              authorizations: nil,
-              root_base_path: true,
-              api_documentation: { desc: 'Swagger compatible API description' },
-              specific_api_documentation: { desc: 'Swagger compatible API description for specific API' }
-            }
-
-            options = defaults.merge(options)
-
-            target_class     = options[:target_class]
-            @@mount_path     = options[:mount_path]
-            @@class_name     = options[:class_name] || options[:mount_path].gsub('/', '')
-            @@markdown       = options[:markdown] ? GrapeSwagger::Markdown.new(options[:markdown]) : nil
-            @@hide_format    = options[:hide_format]
-            api_version      = options[:api_version]
-            authorizations   = options[:authorizations]
-            root_base_path   = options[:root_base_path]
-            extra_info       = options[:info]
-            api_doc          = options[:api_documentation].dup
-            specific_api_doc = options[:specific_api_documentation].dup
-            @@models         = options[:models] || []
-
-            @@hide_documentation_path = options[:hide_documentation_path]
-
-            if options[:format]
-              [:format, :default_format, :default_error_formatter].each do |method|
-                send(method, options[:format])
-              end
-            end
-
-            desc api_doc.delete(:desc), api_doc
-            get @@mount_path do
-              header['Access-Control-Allow-Origin']   = '*'
-              header['Access-Control-Request-Method'] = '*'
-
-              routes = target_class.combined_routes
-              namespaces = target_class.combined_namespaces
-
-              if @@hide_documentation_path
-                routes.reject! { |route, _value| "/#{route}/".index(parse_path(@@mount_path, nil) << '/') == 0 }
-              end
-
-              routes_array = routes.keys.map do |local_route|
-                next if routes[local_route].all?(&:route_hidden)
-
-                url_format  = '.{format}' unless @@hide_format
-
-                description = namespaces[local_route] && namespaces[local_route].options[:desc]
-                description ||= "Operations about #{local_route.pluralize}"
-
-                {
-                  path: "/#{local_route}#{url_format}",
-                  description: description
-                }
-              end.compact
-
-              output = {
-                apiVersion:     api_version,
-                swaggerVersion: '1.2',
-                produces:       content_types_for(target_class),
-                apis:           routes_array,
-                info:           parse_info(extra_info)
-              }
-
-              output[:authorizations] = authorizations unless authorizations.nil? || authorizations.empty?
-
-              output
-            end
-
-            desc specific_api_doc.delete(:desc), { params: {
-              'name' => {
-                desc: 'Resource name of mounted API',
-                type: 'string',
-                required: true
-              }
-            }.merge(specific_api_doc.delete(:params) || {}) }.merge(specific_api_doc)
-
-            get "#{@@mount_path}/:name" do
-              header['Access-Control-Allow-Origin']   = '*'
-              header['Access-Control-Request-Method'] = '*'
-
-              models = []
-              routes = target_class.combined_routes[params[:name]]
-              error!('Not Found', 404) unless routes
-
-              ops = routes.reject(&:route_hidden).group_by do |route|
-                parse_path(route.route_path, api_version)
-              end
-
-              error!('Not Found', 404) unless ops.any?
-
-              apis = []
-
-              ops.each do |path, op_routes|
-                operations = op_routes.map do |route|
-                  notes       = as_markdown(route.route_notes)
-
-                  http_codes  = parse_http_codes(route.route_http_codes, models)
-
-                  models |= @@models if @@models.present?
-
-                  models |= [route.route_entity] if route.route_entity.present?
-
-                  models = models_with_included_presenters(models.flatten.compact)
-
-                  operation = {
-                    notes: notes.to_s,
-                    summary: route.route_description || '',
-                    nickname: route.route_nickname || (route.route_method + route.route_path.gsub(/[\/:\(\)\.]/, '-')),
-                    method: route.route_method,
-                    parameters: parse_header_params(route.route_headers) + parse_params(route.route_params, route.route_path, route.route_method),
-                    type: 'void'
-                  }
-                  operation[:authorizations] = route.route_authorizations unless route.route_authorizations.nil? || route.route_authorizations.empty?
-                  if operation[:parameters].any? { | param | param[:type] == 'File' }
-                    operation.merge!(consumes: ['multipart/form-data'])
-                  end
-                  operation.merge!(responseMessages: http_codes) unless http_codes.empty?
-
-                  if route.route_entity
-                    type = parse_entity_name(route.route_entity)
-                    if route.instance_variable_get(:@options)[:is_array]
-                      operation.merge!(
-                        'type' => 'array',
-                        'items' => generate_typeref(type)
-                      )
-                    else
-                      operation.merge!('type' => type)
-                    end
-                  end
-
-                  operation[:nickname] = route.route_nickname if route.route_nickname
-                  operation
-                end.compact
-                apis << {
-                  path: path,
-                  operations: operations
-                }
-              end
-
-              api_description = {
-                apiVersion:     api_version,
-                swaggerVersion: '1.2',
-                resourcePath:   "/#{params[:name]}",
-                produces:       content_types_for(target_class),
-                apis:           apis
-              }
-
-              base_path                        = parse_base_path(options[:base_path], request)
-              api_description[:basePath]       = base_path if base_path && base_path.size > 0 && root_base_path != false
-              api_description[:models]         = parse_entity_models(models) unless models.empty?
-              api_description[:authorizations] = authorizations if authorizations
-
-              api_description
-            end
-          end
-
-          helpers do
 
             def as_markdown(description)
               description && @@markdown ? @@markdown.as_markdown(strip_heredoc(description)) : description
@@ -259,7 +89,7 @@ module Grape
                                 when 'Numeric'
                                   'double'
                                 else
-                                  parse_entity_name(raw_data_type)
+                                  @@documentation_class.parse_entity_name(raw_data_type)
                                 end
                 description   = value.is_a?(Hash) ? value[:desc] || value[:description] : ''
                 required      = value.is_a?(Hash) ? !!value[:required] : false
@@ -488,6 +318,176 @@ module Grape
                 URI(base_path).relative? ? URI.join(request.base_url, base_path).to_s : base_path
               else
                 request.base_url
+              end
+            end
+
+            def setup(options)
+              defaults = {
+                target_class: nil,
+                mount_path: '/swagger_doc',
+                base_path: nil,
+                api_version: '0.1',
+                markdown: nil,
+                hide_documentation_path: false,
+                hide_format: false,
+                format: nil,
+                models: [],
+                info: {},
+                authorizations: nil,
+                root_base_path: true,
+                api_documentation: { desc: 'Swagger compatible API description' },
+                specific_api_documentation: { desc: 'Swagger compatible API description for specific API' }
+              }
+
+              options = defaults.merge(options)
+
+              target_class     = options[:target_class]
+              @@mount_path     = options[:mount_path]
+              @@class_name     = options[:class_name] || options[:mount_path].gsub('/', '')
+              @@markdown       = options[:markdown] ? GrapeSwagger::Markdown.new(options[:markdown]) : nil
+              @@hide_format    = options[:hide_format]
+              api_version      = options[:api_version]
+              authorizations   = options[:authorizations]
+              root_base_path   = options[:root_base_path]
+              extra_info       = options[:info]
+              api_doc          = options[:api_documentation].dup
+              specific_api_doc = options[:specific_api_documentation].dup
+              @@models         = options[:models] || []
+
+              @@hide_documentation_path = options[:hide_documentation_path]
+
+              if options[:format]
+                [:format, :default_format, :default_error_formatter].each do |method|
+                  send(method, options[:format])
+                end
+              end
+
+              @@documentation_class = self
+
+              desc api_doc.delete(:desc), api_doc
+              get @@mount_path do
+                header['Access-Control-Allow-Origin']   = '*'
+                header['Access-Control-Request-Method'] = '*'
+
+                routes = target_class.combined_routes
+                namespaces = target_class.combined_namespaces
+
+                if @@hide_documentation_path
+                  routes.reject! { |route, _value| "/#{route}/".index(@@documentation_class.parse_path(@@mount_path, nil) << '/') == 0 }
+                end
+
+                routes_array = routes.keys.map do |local_route|
+                  next if routes[local_route].all?(&:route_hidden)
+
+                  url_format  = '.{format}' unless @@hide_format
+
+                  description = namespaces[local_route] && namespaces[local_route].options[:desc]
+                  description ||= "Operations about #{local_route.pluralize}"
+
+                  {
+                    path: "/#{local_route}#{url_format}",
+                    description: description
+                  }
+                end.compact
+
+                output = {
+                  apiVersion:     api_version,
+                  swaggerVersion: '1.2',
+                  produces:       @@documentation_class.content_types_for(target_class),
+                  apis:           routes_array,
+                  info:           @@documentation_class.parse_info(extra_info)
+                }
+
+                output[:authorizations] = authorizations unless authorizations.nil? || authorizations.empty?
+
+                output
+              end
+
+              desc specific_api_doc.delete(:desc), { params: {
+                'name' => {
+                  desc: 'Resource name of mounted API',
+                  type: 'string',
+                  required: true
+                }
+              }.merge(specific_api_doc.delete(:params) || {}) }.merge(specific_api_doc)
+
+              get "#{@@mount_path}/:name" do
+                header['Access-Control-Allow-Origin']   = '*'
+                header['Access-Control-Request-Method'] = '*'
+
+                models = []
+                routes = target_class.combined_routes[params[:name]]
+                error!('Not Found', 404) unless routes
+
+                ops = routes.reject(&:route_hidden).group_by do |route|
+                  @@documentation_class.parse_path(route.route_path, api_version)
+                end
+
+                error!('Not Found', 404) unless ops.any?
+
+                apis = []
+
+                ops.each do |path, op_routes|
+                  operations = op_routes.map do |route|
+                    notes       = @@documentation_class.as_markdown(route.route_notes)
+
+                    http_codes  = @@documentation_class.parse_http_codes(route.route_http_codes, models)
+
+                    models |= @@models if @@models.present?
+
+                    models |= [route.route_entity] if route.route_entity.present?
+
+                    models = @@documentation_class.models_with_included_presenters(models.flatten.compact)
+
+                    operation = {
+                      notes: notes.to_s,
+                      summary: route.route_description || '',
+                      nickname: route.route_nickname || (route.route_method + route.route_path.gsub(/[\/:\(\)\.]/, '-')),
+                      method: route.route_method,
+                      parameters: @@documentation_class.parse_header_params(route.route_headers) + @@documentation_class.parse_params(route.route_params, route.route_path, route.route_method),
+                      type: 'void'
+                    }
+                    operation[:authorizations] = route.route_authorizations unless route.route_authorizations.nil? || route.route_authorizations.empty?
+                    if operation[:parameters].any? { | param | param[:type] == 'File' }
+                      operation.merge!(consumes: ['multipart/form-data'])
+                    end
+                    operation.merge!(responseMessages: http_codes) unless http_codes.empty?
+
+                    if route.route_entity
+                      type = @@documentation_class.parse_entity_name(route.route_entity)
+                      if route.instance_variable_get(:@options)[:is_array]
+                        operation.merge!(
+                          'type' => 'array',
+                          'items' => generate_typeref(type)
+                        )
+                      else
+                        operation.merge!('type' => type)
+                      end
+                    end
+
+                    operation[:nickname] = route.route_nickname if route.route_nickname
+                    operation
+                  end.compact
+                  apis << {
+                    path: path,
+                    operations: operations
+                  }
+                end
+
+                api_description = {
+                  apiVersion:     api_version,
+                  swaggerVersion: '1.2',
+                  resourcePath:   "/#{params[:name]}",
+                  produces:       @@documentation_class.content_types_for(target_class),
+                  apis:           apis
+                }
+
+                base_path                        = @@documentation_class.parse_base_path(options[:base_path], request)
+                api_description[:basePath]       = base_path if base_path && base_path.size > 0 && root_base_path != false
+                api_description[:models]         = @@documentation_class.parse_entity_models(models) unless models.empty?
+                api_description[:authorizations] = authorizations if authorizations
+
+                api_description
               end
             end
           end

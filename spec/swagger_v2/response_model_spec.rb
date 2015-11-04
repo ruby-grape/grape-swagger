0a6c5aa90214ce47d1478787045867e1451e2951
Peter Scholz <peter.scholz@sage.com>
2015-11-04 14:44:23 +0100

    respects given entity key of desc block; corrects exposing of type

--- spec/swagger_v2/response_model_spec.rb
+++ spec/swagger_v2/response_model_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'responseModel' do
   before :all do
-    module MyAPI
+    module ThisApi
       module Entities
         class Kind < Grape::Entity
           expose :title, documentation: { type: 'string', desc: 'Title of the kind.' }
@@ -23,17 +23,17 @@ describe 'responseModel' do
 
         class Something < Grape::Entity
           expose :text, documentation: { type: 'string', desc: 'Content of something.' }
-          expose :kind, using: Kind, documentation: { type: 'MyAPI::Kind', desc: 'The kind of this something.' }
+          expose :kind, using: Kind, documentation: { type: 'ThisApi::Kind', desc: 'The kind of this something.' }
           expose :kind2, using: Kind, documentation: { desc: 'Secondary kind.' }
-          expose :kind3, using: MyAPI::Entities::Kind, documentation: { desc: 'Tertiary kind.' }
-          expose :tags, using: MyAPI::Entities::Tag, documentation: { desc: 'Tags.', is_array: true }
-          expose :relation, using: MyAPI::Entities::Relation, documentation: { type: 'MyAPI::Relation', desc: 'A related model.' }
+          expose :kind3, using: ThisApi::Entities::Kind, documentation: { desc: 'Tertiary kind.' }
+          expose :tags, using: ThisApi::Entities::Tag, documentation: { desc: 'Tags.', is_array: true }
+          expose :relation, using: ThisApi::Entities::Relation, documentation: { type: 'ThisApi::Relation', desc: 'A related model.' }
         end
       end
 
       class ResponseModelApi < Grape::API
         format :json
-        desc 'This returns something or an error',
+        desc 'This returns something',
           is_array: true,
           http_codes: [ { code: 200, message: 'OK', model: Entities::Something } ]
         get '/something' do
@@ -67,7 +67,7 @@ describe 'responseModel' do
   end
 
   def app
-    MyAPI::ResponseModelApi
+    ThisApi::ResponseModelApi
   end
 
   subject do
@@ -135,5 +135,75 @@ describe 'responseModel' do
     expect(subject['definitions']['Tag']).to eq(
       "type"=>"object", "properties"=>{"name"=>{"type"=>"string"}}
     )
+  end
+end
+
+describe 'should build definition from given entity' do
+  before :all do
+    module TheseApi
+      module Entities
+        class Kind < Grape::Entity
+          expose :id, documentation: { type: Integer, desc: 'Title of the kind.' }
+        end
+
+        class Relation < Grape::Entity
+          expose :name, documentation: { type: String, desc: 'Name' }
+
+        end
+        class Tag < Grape::Entity
+          expose :name, documentation: { type: 'string', desc: 'Name' }
+
+        end
+
+        class SomeEntity < Grape::Entity
+          expose :text, documentation: { type: 'string', desc: 'Content of something.' }
+          expose :kind, using: Kind, documentation: { type: 'TheseApi::Kind', desc: 'The kind of this something.' }
+          expose :kind2, using: Kind, documentation: { desc: 'Secondary kind.' }
+          expose :kind3, using: TheseApi::Entities::Kind, documentation: { desc: 'Tertiary kind.' }
+          expose :tags, using: TheseApi::Entities::Tag, documentation: { desc: 'Tags.', is_array: true }
+          expose :relation, using: TheseApi::Entities::Relation, documentation: { type: 'TheseApi::Relation', desc: 'A related model.' }
+        end
+      end
+
+      class ResponseEntityApi < Grape::API
+        format :json
+        desc 'This returns something',
+          is_array: true,
+          entity: Entities::SomeEntity
+        get '/some_entity' do
+          something = OpenStruct.new text: 'something'
+          present something, with: Entities::SomeEntity
+        end
+
+        add_swagger_documentation
+      end
+    end
+  end
+
+  def app
+    TheseApi::ResponseEntityApi
+  end
+
+  subject do
+    get '/swagger_doc'
+    JSON.parse(last_response.body)
+  end
+
+  it "it prefer entity over others" do
+    expect(subject['definitions']).to eql({
+      "Kind"=>{"type"=>"object", "properties"=>{"id"=>{"type"=>"integer"}}},
+      "Tag"=>{"type"=>"object", "properties"=>{"name"=>{"type"=>"string"}}},
+      "Relation"=>{"type"=>"object", "properties"=>{"name"=>{"type"=>"string"}}},
+      "SomeEntity"=>{
+        "type"=>"object",
+        "properties"=>{
+          "text"=>{"type"=>"string"},
+          "kind"=>{"$ref"=>"#/definitions/Kind"},
+          "kind2"=>{"$ref"=>"#/definitions/Kind"},
+          "kind3"=>{"$ref"=>"#/definitions/Kind"},
+          "tags"=>{"$ref"=>"#/definitions/Tag"},
+          "relation"=>{"$ref"=>"#/definitions/Relation"}}}
+    })
+
   end
 end

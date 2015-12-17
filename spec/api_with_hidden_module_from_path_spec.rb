9177d4d4e8840724efe8379bc2f303de45f4aa02
Peter Philips <peter@planet.io>
2015-12-17 12:48:58 -0800

    fix regex for rubocop

--- spec/api_with_hidden_module_from_path_spec.rb
+++ spec/api_with_hidden_module_from_path_spec.rb
@@ -48,7 +48,7 @@ describe 'Api with "path" versioning and using modules' do
     it 'retrieves swagger-documentation on /swagger_doc/resources that contains :resources api path with version' do
       get '/v1/swagger_doc/resources'
 
-      expect(json_body['apis'][0]['path']).to match(/^\/v1\/resources/)
+      expect(json_body['apis'][0]['path']).to match(%r{/v1/resources})
     end
   end
 
@@ -58,7 +58,7 @@ describe 'Api with "path" versioning and using modules' do
     it 'retrieves swagger-documentation on /swagger_doc/resources that contains :resources api path without version' do
       get '/v1/swagger_doc/resources'
 
-      expect(json_body['apis'][0]['path']).to match(/^\/resources/)
+      expect(json_body['apis'][0]['path']).to match(%r{/resources})
     end
   end
 end

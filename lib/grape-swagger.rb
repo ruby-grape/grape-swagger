e7e20e17af6188413b3b113954569cde82ad47e3
itoufo <yuho.ito@walker.co.jp>
2016-02-07 23:40:08 +0900

    support nested array

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -52,11 +52,12 @@ module Grape
 
       def combine_namespaces(app)
         app.endpoints.each do |endpoint|
-          ns = if endpoint.respond_to?(:namespace_stackable)
-                 endpoint.namespace_stackable(:namespace).last
-               else
-                 endpoint.settings.stack.last[:namespace]
-               end
+          ns =
+            if endpoint.respond_to?(:namespace_stackable)
+              endpoint.namespace_stackable(:namespace).last
+            else
+              endpoint.settings.stack.last[:namespace]
+            end
           # use the full namespace here (not the latest level only)
           # and strip leading slash
           @target_class.combined_namespaces[endpoint.namespace.sub(/^\//, '')] = ns if ns
@@ -150,23 +151,27 @@ module Grape
 
       def parse_array_params(params)
         modified_params = {}
-        array_param = nil
+        array_keys = []
         params.each_key do |k|
+          new_key, modified_params = parse_array_key(array_keys, k, modified_params)
           if params[k].is_a?(Hash) && params[k][:type] == 'Array'
-            array_param = k
-            modified_params[k] = params[k]
-          else
-            new_key = k
-            unless array_param.nil?
-              if k.to_s.start_with?(array_param.to_s + '[')
-                new_key = array_param.to_s + '[]' + k.to_s.split(array_param)[1]
-                modified_params.delete array_param
-              end
-            end
-            modified_params[new_key] = params[k]
+            array_keys.push(new_key)
           end
+          modified_params[new_key] = params[k]
         end
         modified_params
+      end
+
+      def parse_array_key(array_keys, key, modified_params)
+        unless array_keys.blank?
+          array_keys.each do |array_key|
+            if key.to_s.start_with?(array_key.to_s + '[')
+              key = array_key.to_s + '[]' + key.to_s.split(array_key)[1]
+              modified_params.delete array_key
+            end
+          end
+        end
+        [key, modified_params]
       end
 
       def parse_enum_or_range_values(values)

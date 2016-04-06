a0a3d2325f9914510098714e4e688bfa0a04fff2
LeFnord <pscholz.le@gmail.com>
2016-04-07 00:08:32 +0200

    1. part of param type `body` handling

---
+++ lib/grape-swagger/doc_methods/status_codes.rb
@@ -0,0 +1,17 @@
+module GrapeSwagger
+  module DocMethods
+    class StatusCodes
+      class << self
+        def get
+          {
+            get: { code: 200, message: 'get {item}(s)' },
+            post: { code: 201, message: 'created {item}' },
+            put: { code: 200, message: 'updated {item}' },
+            patch: { code: 200, message: 'patched {item}' },
+            delete: { code: 200, message: 'deleted {item}' }
+          }
+        end
+      end
+    end
+  end
+end

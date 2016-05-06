62f61d52ae3163bd7e3c73dcca7a2a563e856852
Kirill Zaitsev <kirik910@gmail.com>
2016-05-06 12:52:15 +0300

    Added head method type

--- lib/grape-swagger/doc_methods/status_codes.rb
+++ lib/grape-swagger/doc_methods/status_codes.rb
@@ -8,7 +8,8 @@ module GrapeSwagger
             post: { code: 201, message: 'created {item}' },
             put: { code: 200, message: 'updated {item}' },
             patch: { code: 200, message: 'patched {item}' },
-            delete: { code: 200, message: 'deleted {item}' }
+            delete: { code: 200, message: 'deleted {item}' },
+            head: { code: 200, message: 'head {item}' }
           }
         end
       end

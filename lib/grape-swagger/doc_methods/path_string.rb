eca701fad634b797824c9b9051deed1887860136
texpert <branzeanu.aurel@gmail.com>
2016-05-24 04:10:18 +0300

    Not good to inject anything into route.path. A copy into the local variable is better. .

--- lib/grape-swagger/doc_methods/path_string.rb
+++ lib/grape-swagger/doc_methods/path_string.rb
@@ -3,25 +3,26 @@ module GrapeSwagger
     class PathString
       class << self
         def build(route, options = {})
+          path = route.path
           # always removing format
-          route.path.sub!(/\(\.\w+?\)$/, '')
-          route.path.sub!('(.:format)', '')
+          path.sub!(/\(\.\w+?\)$/, '')
+          path.sub!('(.:format)', '')
 
           # ... format path params
-          route.path.gsub!(/:(\w+)/, '{\1}')
+          path.gsub!(/:(\w+)/, '{\1}')
 
           # set item from path, this could be used for the definitions object
-          item = route.path.gsub(%r{/{(.+?)}}, '').split('/').last.singularize.underscore.camelize || 'Item'
+          item = path.gsub(%r{/{(.+?)}}, '').split('/').last.singularize.underscore.camelize || 'Item'
 
           if route.version && options[:add_version]
-            route.path.sub!('{version}', route.version.to_s)
+            path.sub!('{version}', route.version.to_s)
           else
-            route.path.sub!('/{version}', '')
+            path.sub!('/{version}', '')
           end
 
-          route.path.prepend(GrapeSwagger::DocMethods::OptionalObject.build(:base_path, options)) if options[:add_base_path]
+          path = "#{GrapeSwagger::DocMethods::OptionalObject.build(:base_path, options)}#{path}" if options[:add_base_path]
 
-          [item, route.path.start_with?('/') ? route.path : "/#{route.path}"]
+          [item, path.start_with?('/') ? path : "/#{path}"]
         end
       end
     end

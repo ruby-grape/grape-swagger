aed9024a846468939f6b884de5d5ddcf2ba5ccd5
Joakim Antman <joakim.antman@visma.com>
2016-06-06 08:32:12 +0300

    Test for tags in a versioned endpoint

--- spec/swagger_v2/endpoint_versioned_path_spec.rb
+++ spec/swagger_v2/endpoint_versioned_path_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe 'Grape::Endpoint#path_and_definitions' do
-  before do
-    module API
-      module V1
-        class Item < Grape::API
-          version 'v1', using: :path
+  let(:api) do
+    item = Class.new(Grape::API) do
+      version 'v1', using: :path
 
-          resource :item do
-            get '/'
-          end
-        end
-      end
-
-      class Root < Grape::API
-        mount API::V1::Item
-        add_swagger_documentation add_version: true
+      resource :item do
+        get '/'
       end
     end
 
-    @options = { add_version: true }
-    @target_routes = API::Root.combined_namespace_routes
+    Class.new(Grape::API) do
+      mount item
+      add_swagger_documentation add_version: true
+    end
   end
 
+  let(:options) { { add_version: true } }
+  let(:target_routes) { api.combined_namespace_routes }
+
+  subject { api.endpoints[0].path_and_definition_objects(target_routes, options) }
+
   it 'is returning a versioned path' do
-    expect(API::V1::Item.endpoints[0]
-      .path_and_definition_objects(@target_routes, @options)[0].keys[0]).to eql '/v1/item'
+    expect(subject[0].keys[0]).to eq '/v1/item'
+  end
+
+  it 'tags the endpoint with the resource name' do
+    pending 'BUG: This is set to the first part of the path'
+    expect(subject.first['/v1/item'][:get][:tags]).to eq ['items']
   end
 end

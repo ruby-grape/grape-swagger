4e29062684d39b3e1a00287d5087e87288b79609
romain.sagean <romain.sagean@hupi.fr>
2016-06-09 10:59:15 +0200

    rubocop style

--- spec/swagger_v2/param_multi_type_spec.rb
+++ spec/swagger_v2/param_multi_type_spec.rb
@@ -1,43 +1,11 @@
 require 'spec_helper'
 
 if Grape::VERSION >= '0.14'
-describe 'Params Multi Types' do
-  def app
-    Class.new(Grape::API) do
-      format :json
-
-      params do
-        requires :input, types: [String, Integer]
-      end
-      post :action do
-      end
-
-      add_swagger_documentation
-    end
-  end
-
-  subject do
-    get '/swagger_doc/action'
-    expect(last_response.status).to eq 200
-    body = JSON.parse last_response.body
-    body['paths']['/action']['post']['parameters']
-  end
-
-  it 'reads param type correctly' do
-    expect(subject).to eq [{
-      'in' => 'formData',
-      'name' => 'input',
-      'type' => 'string',
-      'required' => true
-    }]
-  end
-
-  describe 'header params' do
+  describe 'Params Multi Types' do
     def app
       Class.new(Grape::API) do
         format :json
 
-        desc 'Some API', headers: { 'My-Header' => { required: true, description: 'Set this!' } }
         params do
           requires :input, types: [String, Integer]
         end
@@ -48,10 +16,42 @@ describe 'Params Multi Types' do
       end
     end
 
-    it 'has consistent types' do
-      types = subject.map { |param| param['type'] }
-      expect(types).to eq(%w(string string))
+    subject do
+      get '/swagger_doc/action'
+      expect(last_response.status).to eq 200
+      body = JSON.parse last_response.body
+      body['paths']['/action']['post']['parameters']
+    end
+
+    it 'reads param type correctly' do
+      expect(subject).to eq [{
+        'in' => 'formData',
+        'name' => 'input',
+        'type' => 'string',
+        'required' => true
+      }]
+    end
+
+    describe 'header params' do
+      def app
+        Class.new(Grape::API) do
+          format :json
+
+          desc 'Some API', headers: { 'My-Header' => { required: true, description: 'Set this!' } }
+          params do
+            requires :input, types: [String, Integer]
+          end
+          post :action do
+          end
+
+          add_swagger_documentation
+        end
+      end
+
+      it 'has consistent types' do
+        types = subject.map { |param| param['type'] }
+        expect(types).to eq(%w(string string))
+      end
     end
   end
-end
 end

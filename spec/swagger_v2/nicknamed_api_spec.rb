3bcfc2f47b2993b9c57d21eb3b977c2310352af6
Pablo Bendersky <pbendersky@quadiontech.com>
2016-07-26 13:41:19 -0300

    added support for nicknamed routes

---
+++ spec/swagger_v2/nicknamed_api_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'a nicknamed mounted api' do
+  before :all do
+    class NicknamedMountedApi < Grape::API
+      desc 'Show this endpoint', nickname: 'simple'
+      get '/simple' do
+        { foo: 'bar' }
+      end
+    end
+
+    class NicknamedApi < Grape::API
+      mount NicknamedMountedApi
+      add_swagger_documentation
+    end
+  end
+
+  def app
+    NicknamedApi
+  end
+
+  subject do
+    get '/swagger_doc.json'
+    JSON.parse(last_response.body)
+  end
+
+  it "uses the nickname as the operationId" do
+    expect(subject['paths']['/simple']['get']['operationId']).to eql('simple')
+  end
+end

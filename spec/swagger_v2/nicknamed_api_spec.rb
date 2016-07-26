da1fb55c5f76b8b65ae2eedca272250e358450d1
Pablo Bendersky <pbendersky@gmail.com>
2016-07-26 17:30:38 -0300

    added support for nicknamed routes (#483)
    * added support for nicknamed routes
    
    * uses let instead of method to instantiate the app
    
    * updated spec to use anon class
    
    * fixed rubocop errors
    
    * Refactor to fix rubocop issues.
    
    * Added entry in Changelog.

---
+++ spec/swagger_v2/nicknamed_api_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'a nicknamed mounted api' do
+  def app
+    Class.new(Grape::API) do
+      desc 'Show this endpoint', nickname: 'simple'
+      get '/simple' do
+        { foo: 'bar' }
+      end
+
+      add_swagger_documentation format: :json
+    end
+  end
+
+  subject do
+    get '/swagger_doc.json'
+    JSON.parse(last_response.body)
+  end
+
+  it 'uses the nickname as the operationId' do
+    expect(subject['paths']['/simple']['get']['operationId']).to eql('simple')
+  end
+end

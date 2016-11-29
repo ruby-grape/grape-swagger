ca1f35eb59b8dc820ca42edc8aea52c172e3c2db
peter scholz <peter.scholz@sage.com>
2016-11-29 22:30:42 +0100

    fixes #539 and #542; not all of 530 was commited (#544)
    - adds changelog entry

---
+++ spec/issues/542_array_of_type_in_post_body_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require 'grape-entity'
+require 'grape-swagger-entity'
+
+describe '#542 array of type in post params' do
+  let(:app) do
+    Class.new(Grape::API) do
+      namespace :issue_542 do
+        params do
+          requires :logs, type: Array[String], documentation: { param_type: 'body' }
+        end
+
+        post do
+          present params
+        end
+      end
+
+      add_swagger_documentation format: :json
+    end
+  end
+
+  subject do
+    get '/swagger_doc'
+    JSON.parse(last_response.body)
+  end
+
+  let(:parameters) { subject['paths']['/issue_542']['post']['parameters'] }
+
+  specify do
+    expect(parameters).to eql(
+      [
+        {
+          'in' => 'body',
+          'name' => 'logs',
+          'required' => true,
+          'schema' => {
+            'type' => 'array',
+            'items' => {
+              'type' => 'string'
+            }
+          }
+        }
+      ]
+    )
+  end
+end

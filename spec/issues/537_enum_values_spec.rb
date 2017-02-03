e5afd7db08e1b7ecbd15c740659687baa0af9c85
peter scholz <peter.scholz@sage.com>
2017-02-03 17:35:34 +0100

    allows custom format (#576)
    - adds spec for default value
    - adds changelog entry
    - updates UPGRADING

--- spec/issues/537_enum_values_spec.rb
+++ spec/issues/537_enum_values_spec.rb
@@ -5,7 +5,9 @@ describe '#537 enum values spec' do
     Class.new(Grape::API) do
       namespace :issue_539 do
         class Spec < Grape::Entity
-          expose :test_property, documentation: { values: [:foo, :bar] }
+          expose :enum_property, documentation: { values: [:foo, :bar] }
+          expose :enum_property_default, documentation: { values: %w(a b c), default: 'c' }
+          expose :own_format, documentation: { format: 'log' }
         end
 
         desc 'create account',
@@ -23,10 +25,23 @@ describe '#537 enum values spec' do
     JSON.parse(last_response.body)
   end
 
-  let(:property) { subject['definitions']['Spec']['properties']['test_property'] }
-
+  let(:property) { subject['definitions']['Spec']['properties']['enum_property'] }
   specify do
     expect(property).to include 'enum'
     expect(property['enum']).to eql %w(foo bar)
+  end
+
+  let(:property_default) { subject['definitions']['Spec']['properties']['enum_property_default'] }
+  specify do
+    expect(property_default).to include 'enum'
+    expect(property_default['enum']).to eql %w(a b c)
+    expect(property_default).to include 'default'
+    expect(property_default['default']).to eql 'c'
+  end
+
+  let(:own_format) { subject['definitions']['Spec']['properties']['own_format'] }
+  specify do
+    expect(own_format).to include 'format'
+    expect(own_format['format']).to eql 'log'
   end
 end

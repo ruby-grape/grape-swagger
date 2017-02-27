303630071cf38424b1d6366a3390f9711e80246d
risa <richard.korinek@gmail.com>
2017-02-27 23:06:07 +0100

    updates for requested changes - renamed spec and showed operationId value in spec

--- spec/issues/587_range_parameter_delimited_by_dash_spec.rb
+++ spec/issues/587_range_parameter_delimited_by_dash_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe '#XXX nested entity given as string' do
+describe '#587 process route with parameters delimited by dash' do
   let(:app) do
     Class.new(Grape::API) do
       namespace :range_parameter do
@@ -16,9 +16,9 @@ describe '#XXX nested entity given as string' do
 
   subject do
     get '/swagger_doc'
-    result = JSON.parse(last_response.body)['paths']
-    result
+    JSON.parse(last_response.body)['paths']
   end
 
   specify { expect(subject.keys).to include '/range_parameter/range/{range_start}-{range_end}' }
+  specify { expect(subject['/range_parameter/range/{range_start}-{range_end}']['get']['operationId']).to eql 'getRangeParameterRangeRangeStart-RangeEnd' }
 end

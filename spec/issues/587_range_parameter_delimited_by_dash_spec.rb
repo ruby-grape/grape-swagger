79b2059ad6ed2e177929f178ca5b6ad91937393a
Richard Kořínek <richard.korinek@gmail.com>
2017-02-28 01:12:41 +0100

    Parameters delimited by dash cause exception (#586)
    * Fixed range parameters delimited by dash
    
    * fixed rubocop offense
    
    * updates for requested changes - renamed spec and showed operationId value in spec
    
    * Changelog updated

---
+++ spec/issues/587_range_parameter_delimited_by_dash_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe '#587 process route with parameters delimited by dash' do
+  let(:app) do
+    Class.new(Grape::API) do
+      namespace :range_parameter do
+        desc 'Get a array with range'
+        get '/range/:range_start-:range_end' do
+          present []
+        end
+      end
+
+      add_swagger_documentation format: :json
+    end
+  end
+
+  subject do
+    get '/swagger_doc'
+    JSON.parse(last_response.body)['paths']
+  end
+
+  specify { expect(subject.keys).to include '/range_parameter/range/{range_start}-{range_end}' }
+  specify { expect(subject['/range_parameter/range/{range_start}-{range_end}']['get']['operationId']).to eql 'getRangeParameterRangeRangeStart-RangeEnd' }
+end

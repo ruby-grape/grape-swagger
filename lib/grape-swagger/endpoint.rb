8886968b5e6029201c06761281542b96be932bcd
deepak <deepak@kreatio.com>
2017-08-18 21:11:43 +0530

    Corrected error in description of Models (definitions)

--- lib/grape-swagger/endpoint.rb
+++ lib/grape-swagger/endpoint.rb
@@ -211,7 +211,6 @@ module Grape
         next if memo.key?(204)
         next unless !response_model.start_with?('Swagger_doc') && (@definitions[response_model] || value[:model])
 
-        @definitions[response_model][:description] = description_object(route)
         # TODO: proof that the definition exist, if model isn't specified
         reference = { '$ref' => "#/definitions/#{response_model}" }
         memo[value[:code]][:schema] = if route.options[:is_array] && value[:code] < 300

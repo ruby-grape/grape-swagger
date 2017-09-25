5b520e27e8db3be70edbcd21be6e4fdb125c7c87
Wojciech Kocjan <wojciech@kocjan.org>
2017-09-25 16:07:45 +0200

    Added missing tests, based on code from
    https://github.com/ruby-grape/grape-swagger/pull/543 by @milgner
    
    Applied on top of latest master

---
+++ spec/issues/267_nested_namespaces.rb
@@ -0,0 +1,56 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'nested namespaces' do
+  let(:app) do
+    Class.new(Grape::API) do
+      route_param :root do
+        resources :apps do
+          route_param :app_id do
+            resource :build do
+              desc 'Builds an application'
+              post do
+                { name: 'Test' }
+              end
+            end
+          end
+        end
+      end
+
+      add_swagger_documentation version: 'v1'
+    end
+  end
+
+  describe 'combined_namespace_routes' do
+    it 'parses root namespace properly' do
+      expect(app.combined_namespace_routes.keys).to include('apps')
+    end
+  end
+
+  describe '#extract_parent_route' do
+    it 'extracts parent for non-namespaced path properly' do
+      expect(app.send :extract_parent_route, '/apps/:app_id/build').to eq('apps')
+    end
+
+    it 'extracts parent for namespaced path properly' do
+      expect(app.send :extract_parent_route, '/:root/apps/:app_id/build').to eq('apps')
+    end
+  end
+
+  describe 'retrieves swagger-documentation on /swagger_doc' do
+    let(:route_name) { '{root}/apps/{app_id}/build' }
+
+    subject do
+      get '/swagger_doc.json'
+      JSON.parse(last_response.body)
+    end
+
+    context 'paths' do
+      specify do
+        expect(subject['paths'].keys).to include "/#{route_name}"
+      end
+    end
+  end
+end

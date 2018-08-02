f7376f784390f0af7dcf540518a7df8910db83f1
Josh Murphy <jdmurphy@users.noreply.github.com>
2018-08-02 01:41:42 -0500

    Fix is_array check for non-contiguous sub and parent entities (#691)

--- spec/lib/endpoint_spec.rb
+++ spec/lib/endpoint_spec.rb
@@ -7,19 +7,6 @@ describe Grape::Endpoint do
     described_class.new(Grape::Util::InheritableSetting.new, path: '/', method: :get)
   end
 
-  describe '#param_type_is_array?' do
-    it 'returns true if the value passed represents an array' do
-      expect(subject.send(:param_type_is_array?, 'Array')).to be_truthy
-      expect(subject.send(:param_type_is_array?, '[String]')).to be_truthy
-      expect(subject.send(:param_type_is_array?, 'Array[Integer]')).to be_truthy
-    end
-
-    it 'returns false if the value passed does not represent an array' do
-      expect(subject.send(:param_type_is_array?, 'String')).to be_falsey
-      expect(subject.send(:param_type_is_array?, '[String, Integer]')).to be_falsey
-    end
-  end
-
   describe '.content_types_for' do
     describe 'defined on target_class' do
       let(:own_json) { 'text/own-json' }
@@ -54,6 +41,123 @@ describe Grape::Endpoint do
           specify do
             expect(object).to eql %w[application/xml application/json text/plain]
           end
+        end
+      end
+    end
+  end
+
+  describe '#param_type_is_array?' do
+    it 'returns true if the value passed represents an array' do
+      expect(subject.send(:param_type_is_array?, 'Array')).to be_truthy
+      expect(subject.send(:param_type_is_array?, '[String]')).to be_truthy
+      expect(subject.send(:param_type_is_array?, 'Array[Integer]')).to be_truthy
+    end
+
+    it 'returns false if the value passed does not represent an array' do
+      expect(subject.send(:param_type_is_array?, 'String')).to be_falsey
+      expect(subject.send(:param_type_is_array?, '[String, Integer]')).to be_falsey
+    end
+  end
+
+  describe 'parse_request_params' do
+    before do
+      subject.send(:parse_request_params, params)
+    end
+
+    context 'when params do not contain an array' do
+      let(:params) do
+        [
+          ['id', { required: true, type: 'String' }],
+          ['description', { required: false, type: 'String' }]
+        ]
+      end
+
+      let(:expected_params) do
+        [
+          ['id', { required: true, type: 'String' }],
+          ['description', { required: false, type: 'String' }]
+        ]
+      end
+
+      it 'parses params correctly' do
+        expect(params).to eq expected_params
+      end
+    end
+
+    context 'when params contain a simple array' do
+      let(:params) do
+        [
+          ['id', { required: true, type: 'String' }],
+          ['description', { required: false, type: 'String' }],
+          ['stuffs', { required: true, type: 'Array[String]' }]
+        ]
+      end
+
+      let(:expected_params) do
+        [
+          ['id', { required: true, type: 'String' }],
+          ['description', { required: false, type: 'String' }],
+          ['stuffs', { required: true, type: 'Array[String]', is_array: true }]
+        ]
+      end
+
+      it 'parses params correctly and adds is_array to the array' do
+        expect(params).to eq expected_params
+      end
+    end
+
+    context 'when params contain a complex array' do
+      let(:params) do
+        [
+          ['id', { required: true, type: 'String' }],
+          ['description', { required: false, type: 'String' }],
+          ['stuffs', { required: true, type: 'Array' }],
+          ['stuffs[id]', { required: true, type: 'String' }]
+        ]
+      end
+
+      let(:expected_params) do
+        [
+          ['id', { required: true, type: 'String' }],
+          ['description', { required: false, type: 'String' }],
+          ['stuffs', { required: true, type: 'Array', is_array: true }],
+          ['stuffs[id]', { required: true, type: 'String', is_array: true }]
+        ]
+      end
+
+      it 'parses params correctly and adds is_array to the array and all elements' do
+        expect(params).to eq expected_params
+      end
+
+      context 'when array params are not contiguous with parent array' do
+        let(:params) do
+          [
+            ['id', { required: true, type: 'String' }],
+            ['description', { required: false, type: 'String' }],
+            ['stuffs', { required: true, type: 'Array' }],
+            ['stuffs[owners]', { required: true, type: 'Array' }],
+            ['stuffs[creators]', { required: true, type: 'Array' }],
+            ['stuffs[owners][id]', { required: true, type: 'String' }],
+            ['stuffs[creators][id]', { required: true, type: 'String' }],
+            ['stuffs_and_things', { required: true, type: 'String' }]
+          ]
+        end
+
+        let(:expected_params) do
+          [
+            ['id', { required: true, type: 'String' }],
+            ['description', { required: false, type: 'String' }],
+            ['stuffs', { required: true, type: 'Array', is_array: true }],
+            ['stuffs[owners]', { required: true, type: 'Array', is_array: true }],
+            ['stuffs[creators]', { required: true, type: 'Array', is_array: true }],
+            ['stuffs[owners][id]', { required: true, type: 'String', is_array: true }],
+            ['stuffs[creators][id]', { required: true, type: 'String', is_array: true }],
+            ['stuffs_and_things', { required: true, type: 'String' }]
+          ]
+        end
+
+        it 'parses params correctly and adds is_array to the array and all elements' do
+          expect(params).to eq expected_params
         end
       end
     end

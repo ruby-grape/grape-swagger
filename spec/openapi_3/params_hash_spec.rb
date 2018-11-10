fbdc7f6afdda04bbd0efb2b9326150b4a04381d2
blake <blake.pettersson@badiapp.com>
2018-11-10 14:28:34 +0100

    Initial support for openapi3 requestBody

---
+++ spec/openapi_3/params_hash_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Group Params as Hash' do
+  def app
+    Class.new(Grape::API) do
+      format :json
+
+      params do
+        requires :required_group, type: Hash do
+          requires :required_param_1
+          requires :required_param_2
+        end
+      end
+      post '/use_groups' do
+        { 'declared_params' => declared(params) }
+      end
+
+      params do
+        requires :typed_group, type: Hash do
+          requires :id, type: Integer, desc: 'integer given'
+          requires :name, type: String, desc: 'string given'
+          optional :email, type: String, desc: 'email given'
+          optional :others, type: Integer, values: [1, 2, 3]
+        end
+      end
+      post '/use_given_type' do
+        { 'declared_params' => declared(params) }
+      end
+
+      add_swagger_documentation openapi_version: '3.0'
+    end
+  end
+
+  describe 'grouped parameters' do
+    subject do
+      get '/swagger_doc/use_groups'
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['paths']['/use_groups']['post']).to include('requestBody')
+      expect(subject['paths']['/use_groups']['post']['requestBody']['content']).to eql(
+        'application/x-www-form-urlencoded' => {
+          'schema' => {
+            'properties' => {
+              'required_group[required_param_1]' => { 'type' => 'string' },
+              'required_group[required_param_2]' => { 'type' => 'string' }
+            },
+            'required' => %w(required_group[required_param_1] required_group[required_param_2]),
+            'type' => 'object'
+          }
+        }
+      )
+    end
+  end
+
+  describe 'grouped parameters with given type' do
+    subject do
+      get '/swagger_doc/use_given_type'
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['paths']['/use_given_type']['post']).to include('requestBody')
+      expect(subject['paths']['/use_given_type']['post']['requestBody']['content']).to eql(
+        'application/x-www-form-urlencoded' => {
+          'schema' => {
+            'properties' => {
+              'typed_group[email]' => { 'description' => 'email given', 'type' => 'string' },
+              'typed_group[id]' => { 'description' => 'integer given', 'format' => 'int32', 'type' => 'integer' },
+              'typed_group[name]' => { 'description' => 'string given', 'type' => 'string' },
+              'typed_group[others]' => { 'enum' => [1, 2, 3], 'format' => 'int32', 'type' => 'integer' }
+            },
+            'required' => ['typed_group[id]', 'typed_group[name]'],
+            'type' => 'object'
+          }
+        }
+      )
+    end
+  end
+end

1c85b58de47f52ec8db9bfd2582946cdebeca10e
blake <blake.pettersson@badiapp.com>
2018-11-25 15:01:17 +0100

    Add test

---
+++ spec/openapi_3/openapi_3_headers_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'headers' do
+  include_context "#{MODEL_PARSER} swagger example"
+
+  before :all do
+    module TheApi
+      class HeadersApi < Grape::API
+        format :json
+
+        desc 'This returns something',
+             failure: [{ code: 400, model: Entities::ApiError }],
+             headers:  {
+               'X-Rate-Limit-Limit' => {
+                 'description' => 'The number of allowed requests in the current period',
+                 'type' => 'integer'
+               }
+             },
+
+             entity: Entities::UseResponse
+        params do
+          optional :param_x, type: String, desc: 'This is a parameter', documentation: { param_type: 'query' }
+        end
+        get '/use_headers' do
+          { 'declared_params' => declared(params) }
+        end
+
+        add_swagger_documentation openapi_version: '3.0'
+      end
+    end
+  end
+
+  def app
+    TheApi::HeadersApi
+  end
+
+  subject do
+    get '/swagger_doc'
+    JSON.parse(last_response.body)
+  end
+
+  specify do
+    parameters = subject['paths']['/use_headers']['get']['parameters']
+    expect(parameters).to include(
+      'in' => 'header',
+      'name' => 'X-Rate-Limit-Limit',
+      'description' => 'The number of allowed requests in the current period',
+      'schema' => { 'format' => 'int32', 'type' => 'integer' },
+      'required' => false
+    )
+    expect(parameters.size).to eq(2)
+    expect(parameters.first['in']).to eq('header')
+    expect(parameters.last['in']).to eq('query')
+  end
+end

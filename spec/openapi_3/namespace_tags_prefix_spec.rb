736aa14bc9b4caf4ab7f2f5931d62951e2eeb15d
blake <blake.pettersson@badiapp.com>
2018-12-27 16:28:01 +0100

    Add specs

---
+++ spec/openapi_3/namespace_tags_prefix_spec.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'namespace tags check while using prefix and version' do
+  include_context 'namespace example'
+
+  before :all do
+    module TheApi
+      class NamespaceApi < Grape::API
+        version %i[v1 v2]
+      end
+
+      class CascadingVersionApi < Grape::API
+        version :v2
+
+        namespace :hudson do
+          desc 'Document root'
+          get '/' do
+          end
+        end
+
+        namespace :colorado do
+          desc 'This gets something.',
+               notes: '_test_'
+
+          get '/simple' do
+            { bla: 'something' }
+          end
+        end
+      end
+    end
+
+    class TagApi < Grape::API
+      prefix :api
+      mount TheApi::CascadingVersionApi
+      mount TheApi::NamespaceApi
+      add_swagger_documentation openapi_version: '3.0'
+    end
+  end
+
+  def app
+    TagApi
+  end
+
+  describe 'retrieves swagger-documentation on /swagger_doc' do
+    subject do
+      get '/api/swagger_doc.json'
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['tags']).to eql(
+        [
+          { 'name' => 'hudson', 'description' => 'Operations about hudsons' },
+          { 'name' => 'colorado', 'description' => 'Operations about colorados' },
+          { 'name' => 'thames', 'description' => 'Operations about thames' },
+          { 'name' => 'niles', 'description' => 'Operations about niles' }
+        ]
+      )
+
+      expect(subject['paths']['/api/v1/hudson']['get']['tags']).to eql(['hudson'])
+      expect(subject['paths']['/api/v1/colorado/simple']['get']['tags']).to eql(['colorado'])
+      expect(subject['paths']['/api/v1/colorado/simple-test']['get']['tags']).to eql(['colorado'])
+      expect(subject['paths']['/api/v1/thames/simple_with_headers']['get']['tags']).to eql(['thames'])
+      expect(subject['paths']['/api/v1/niles/items']['post']['tags']).to eql(['niles'])
+      expect(subject['paths']['/api/v1/niles/custom']['get']['tags']).to eql(['niles'])
+      expect(subject['paths']['/api/v2/hudson']['get']['tags']).to eql(['hudson'])
+      expect(subject['paths']['/api/v2/colorado/simple']['get']['tags']).to eql(['colorado'])
+    end
+  end
+
+  describe 'retrieves the documentation for mounted-api' do
+    subject do
+      get '/api/swagger_doc/colorado.json'
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['tags']).to eql(
+        [
+          { 'name' => 'colorado', 'description' => 'Operations about colorados' }
+        ]
+      )
+
+      expect(subject['paths']['/api/v1/colorado/simple']['get']['tags']).to eql(['colorado'])
+      expect(subject['paths']['/api/v1/colorado/simple-test']['get']['tags']).to eql(['colorado'])
+    end
+
+    describe 'includes headers' do
+      subject do
+        get '/api/swagger_doc/thames.json'
+        JSON.parse(last_response.body)
+      end
+
+      specify do
+        expect(subject['tags']).to eql(
+          [
+            { 'name' => 'thames', 'description' => 'Operations about thames' }
+          ]
+        )
+
+        expect(subject['paths']['/api/v1/thames/simple_with_headers']['get']['tags']).to eql(['thames'])
+      end
+    end
+  end
+end
---
+++ spec/openapi_3/openapi_3_extensions_spec.rb
@@ -0,0 +1,150 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'extensions' do
+  include_context "#{MODEL_PARSER} swagger example"
+
+  before :all do
+    module TheApi
+      class ExtensionsApi < Grape::API
+        format :json
+
+        route_setting :x_path, some: 'stuff'
+
+        desc 'This returns something with extension on path level',
+             params: Entities::UseResponse.documentation,
+             failure: [{ code: 400, message: 'NotFound', model: Entities::ApiError }]
+        get '/path_extension' do
+          { 'declared_params' => declared(params) }
+        end
+
+        route_setting :x_operation, some: 'stuff'
+
+        desc 'This returns something with extension on verb level',
+             params: Entities::UseResponse.documentation,
+             failure: [{ code: 400, message: 'NotFound', model: Entities::ApiError }]
+        params do
+          requires :id, type: Integer
+        end
+        get '/verb_extension' do
+          { 'declared_params' => declared(params) }
+        end
+
+        route_setting :x_def, for: 200, some: 'stuff'
+
+        desc 'This returns something with extension on definition level',
+             params: Entities::ResponseItem.documentation,
+             success: Entities::ResponseItem,
+             failure: [{ code: 400, message: 'NotFound', model: Entities::ApiError }]
+        get '/definitions_extension' do
+          { 'declared_params' => declared(params) }
+        end
+
+        route_setting :x_def, [{ for: 422, other: 'stuff' }, { for: 200, some: 'stuff' }]
+
+        desc 'This returns something with extension on definition level',
+             success: Entities::OtherItem
+        get '/non_existent_status_definitions_extension' do
+          { 'declared_params' => declared(params) }
+        end
+
+        route_setting :x_def, [{ for: 422, other: 'stuff' }, { for: 200, some: 'stuff' }]
+
+        desc 'This returns something with extension on definition level',
+             success: Entities::OtherItem,
+             failure: [{ code: 422, message: 'NotFound', model: Entities::SecondApiError }]
+        get '/multiple_definitions_extension' do
+          { 'declared_params' => declared(params) }
+        end
+
+        add_swagger_documentation(openapi_version: '3.0', x: { some: 'stuff' })
+      end
+    end
+  end
+
+  def app
+    TheApi::ExtensionsApi
+  end
+
+  describe 'extension on root level' do
+    subject do
+      get '/swagger_doc/path_extension'
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject).to include 'x-some'
+      expect(subject['x-some']).to eql 'stuff'
+    end
+  end
+
+  describe 'extension on path level' do
+    subject do
+      get '/swagger_doc/path_extension'
+      puts last_response.body
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['paths']['/path_extension']).to include 'x-some'
+      expect(subject['paths']['/path_extension']['x-some']).to eql 'stuff'
+    end
+  end
+
+  describe 'extension on verb level' do
+    subject do
+      get '/swagger_doc/verb_extension'
+      puts last_response.body
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['paths']['/verb_extension']['get']).to include 'x-some'
+      expect(subject['paths']['/verb_extension']['get']['x-some']).to eql 'stuff'
+    end
+  end
+
+  describe 'extension on definition level' do
+    subject do
+      get '/swagger_doc/definitions_extension'
+      puts last_response.body
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['components']['schemas']['ResponseItem']).to include 'x-some'
+      expect(subject['components']['schemas']['ResponseItem']['x-some']).to eql 'stuff'
+      expect(subject['components']['schemas']['ApiError']).not_to include 'x-some'
+    end
+  end
+
+  describe 'extension on definition level' do
+    subject do
+      get '/swagger_doc/multiple_definitions_extension'
+      puts last_response.body
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['components']['schemas']['OtherItem']).to include 'x-some'
+      expect(subject['components']['schemas']['OtherItem']['x-some']).to eql 'stuff'
+      expect(subject['components']['schemas']['SecondApiError']).to include 'x-other'
+      expect(subject['components']['schemas']['SecondApiError']['x-other']).to eql 'stuff'
+    end
+  end
+
+  describe 'extension on definition level' do
+    subject do
+      get '/swagger_doc/non_existent_status_definitions_extension'
+      puts last_response.body
+      JSON.parse(last_response.body)
+    end
+
+    specify do
+      expect(subject['components']['schemas'].length).to eql 1
+      expect(subject['components']['schemas']['OtherItem']).to include 'x-some'
+      expect(subject['components']['schemas']['OtherItem']['x-some']).to eql 'stuff'
+    end
+  end
+end

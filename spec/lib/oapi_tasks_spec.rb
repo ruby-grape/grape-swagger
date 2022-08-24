34ea856dac6e7646dcebaeb2655a885c11cf310e
Ray Weng <rw422@cornell.edu>
2022-08-24 16:43:37 -0500

    show that rake fetch:oapi generates wrong output filename

--- spec/lib/oapi_tasks_spec.rb
+++ spec/lib/oapi_tasks_spec.rb
@@ -137,6 +137,12 @@ RSpec.describe GrapeSwagger::Rake::OapiTasks do
       it 'returns swagger_doc.json' do
         expect(subject.send(:file, docs_url)).to end_with 'swagger_doc.json'
       end
+      
+      context 'api has version' do
+        it 'returns versioned swagger_doc.json' do
+          expect(File.basename(subject.send(:file, docs_url))).to eq('swagger_doc_v1.json')
+        end
+      end
     end
 
     describe 'store given' do

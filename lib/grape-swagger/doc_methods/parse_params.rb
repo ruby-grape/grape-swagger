9c56c3617a962de6c422ecf17723dd4fca921e44
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2023-03-28 09:31:49 +0200

    Style/NegatedIf: Favor unless over if for negative conditions.

--- lib/grape-swagger/doc_methods/parse_params.rb
+++ lib/grape-swagger/doc_methods/parse_params.rb
@@ -141,7 +141,7 @@ module GrapeSwagger
 
         def document_example(settings)
           example = settings[:example]
-          @parsed_param[:example] = example if !example.nil?
+          @parsed_param[:example] = example unless example.nil?
         end
 
         def param_type(value_type)

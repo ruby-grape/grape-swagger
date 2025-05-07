18770468777a4e0cb1840f667602a87bb7f26c49
Olivier Bellone <olivier@thatch.ai>
2025-05-06 18:25:31 -0700

    Use x-example for non-body parameters

--- spec/swagger_v2/params_example_spec.rb
+++ spec/swagger_v2/params_example_spec.rb
@@ -7,13 +7,25 @@ describe 'Param example' do
     Class.new(Grape::API) do
       format :json
 
-      params do
-        requires :id, type: Integer, documentation: { example: 123 }
-        optional :name, type: String, documentation: { example: 'Person' }
-        optional :obj, type: 'Object', documentation: { example: { 'foo' => 'bar' } }
+      helpers do
+        params :common_params do
+          requires :id, type: Integer, documentation: { example: 123 }
+          optional :name, type: String, documentation: { example: 'Person' }
+          optional :obj, type: 'Object', documentation: { example: { 'foo' => 'bar' } }
+        end
       end
 
-      get '/endpoint_with_examples' do
+      params do
+        use :common_params
+      end
+      get '/endpoint_with_examples/:id' do
+        { 'declared_params' => declared(params) }
+      end
+
+      params do
+        use :common_params
+      end
+      post '/endpoint_with_examples/:id' do
         { 'declared_params' => declared(params) }
       end
 
@@ -27,14 +39,34 @@ describe 'Param example' do
       JSON.parse(last_response.body)
     end
 
-    specify do
-      expect(subject['paths']['/endpoint_with_examples']['get']['parameters']).to eql(
-        [
-          { 'in' => 'query', 'name' => 'id', 'type' => 'integer', 'example' => 123, 'format' => 'int32', 'required' => true },
-          { 'in' => 'query', 'name' => 'name', 'type' => 'string', 'example' => 'Person', 'required' => false },
-          { 'in' => 'query', 'name' => 'obj', 'type' => 'Object', 'example' => { 'foo' => 'bar' }, 'required' => false }
-        ]
-      )
+    describe 'examples for non-body parameters ' do
+      specify do
+        expect(subject['paths']['/endpoint_with_examples/{id}']['get']['parameters']).to eql(
+          [
+            { 'in' => 'path', 'name' => 'id', 'type' => 'integer', 'x-example' => 123, 'format' => 'int32', 'required' => true },
+            { 'in' => 'query', 'name' => 'name', 'type' => 'string', 'x-example' => 'Person', 'required' => false },
+            { 'in' => 'query', 'name' => 'obj', 'type' => 'Object', 'x-example' => { 'foo' => 'bar' }, 'required' => false }
+          ]
+        )
+      end
+    end
+
+    describe 'examples for body parameters' do
+      specify do
+        expect(subject['paths']['/endpoint_with_examples/{id}']['post']['parameters']).to eql(
+          [
+            { 'in' => 'path', 'name' => 'id', 'type' => 'integer', 'x-example' => 123, 'format' => 'int32', 'required' => true },
+            { 'in' => 'body', 'name' => 'postEndpointWithExamplesId', 'schema' => { '$ref' => '#/definitions/postEndpointWithExamplesId' }, 'required' => true }
+          ]
+        )
+        expect(subject['definitions']['postEndpointWithExamplesId']).to eql(
+          'type' => 'object',
+          'properties' => {
+            'name' => { 'type' => 'string', 'example' => 'Person' },
+            'obj' => { 'type' => 'Object', 'example' => { 'foo' => 'bar' } }
+          }
+        )
+      end
     end
   end
 end

136301dcc25b0c370bc461bc347f5d82abca8b2f
Niko M <nikolay.moskvin@gmail.com>
2026-05-11 12:24:22 +1000

    Rename Grape::Entity classes for clarity and update documentation references

--- spec/issues/537_enum_values_spec.rb
+++ spec/issues/537_enum_values_spec.rb
@@ -6,14 +6,14 @@ describe '#537 enum values spec' do
   let(:app) do
     Class.new(Grape::API) do
       namespace :issue_537 do
-        class Spec < Grape::Entity
+        class Issue537Spec < Grape::Entity
           expose :enum_property, documentation: { values: %i[foo bar] }
           expose :enum_property_default, documentation: { values: %w[a b c], default: 'c' }
           expose :own_format, documentation: { format: 'log' }
         end
 
         desc 'create account',
-             success: Spec
+             success: Issue537Spec
         get do
           { message: 'hi' }
         end
@@ -28,13 +28,13 @@ describe '#537 enum values spec' do
     JSON.parse(last_response.body)
   end
 
-  let(:property) { subject['definitions']['Spec']['properties']['enum_property'] }
+  let(:property) { subject['definitions']['Issue537Spec']['properties']['enum_property'] }
   specify do
     expect(property).to include 'enum'
     expect(property['enum']).to eql %w[foo bar]
   end
 
-  let(:property_default) { subject['definitions']['Spec']['properties']['enum_property_default'] }
+  let(:property_default) { subject['definitions']['Issue537Spec']['properties']['enum_property_default'] }
   specify do
     expect(property_default).to include 'enum'
     expect(property_default['enum']).to eql %w[a b c]
@@ -42,7 +42,7 @@ describe '#537 enum values spec' do
     expect(property_default['default']).to eql 'c'
   end
 
-  let(:own_format) { subject['definitions']['Spec']['properties']['own_format'] }
+  let(:own_format) { subject['definitions']['Issue537Spec']['properties']['own_format'] }
   specify do
     expect(own_format).to include 'format'
     expect(own_format['format']).to eql 'log'
